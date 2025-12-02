10f25b2eb0e06c4401f28f866c6b7273f4d73c11
Gaurav Pendharkar <mitugaurav15@gmail.com>
2025-12-01 22:20:58 -0500

    update scripts/postprocess.py: process the batch outputs from together-ai api

--- scripts/postprocess.py
+++ scripts/postprocess.py
@@ -1,2 +1,21 @@
+import json
+import pandas as pd
+
+
 def parse_baseline_llm_results(results_jsonl_path: str) -> pd.DataFrame:
     pass
+
+def parse_reasoning_llm_results(results_jsonl_path: str) -> dict:
+    
+    reasoning = dict()
+    with open(results_jsonl_path, 'r') as f:
+        for line in f:
+            response = json.loads(line.strip())
+            row_id = int(response["custom_id"].split("-")[1])
+            llm_output = response["response"]["body"]["choices"][0]["message"]["content"]
+            reasoning[row_id] = str(llm_output.split("<REASONING>")[1].replace("\n</REASONING>", "").strip())
+    
+    return reasoning
+    
+
+            
